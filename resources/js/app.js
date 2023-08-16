8f50daf9a248d58dc73b354af208f0aa5d555662
Daniel S <sech-daniil@yandex.ru>
2023-08-16 15:13:44 +0300

    fix code style

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,20 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+// ZiggyVue для обращения к роутам ларавел: route('login')
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+// appName -- для отрисовки в браузере title. по умолчанию - Laravel
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - ${appName}`, // Финальный тайтл
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+    setup({el, App, props, plugin}) {
+        return createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .mount(el);
